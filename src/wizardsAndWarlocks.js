adc70abe2c597dd9ad987a6d1bd9e08511244d6b
Ian Gregg <iangregg188@gmail.com>
2020-06-24 11:52:52 -0700

    have refactored code within the JS file.
    co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

--- src/wizardsAndWarlocks.js
+++ src/wizardsAndWarlocks.js
@@ -1,18 +1,20 @@
 export class Character {
   constructor(type) {
     this.type = type; 
+    this.health = 10;
+    this.abilities(this.type);
+  }
+
+  abilities(type) {
     if (this.type === "wizard") {
-      this.health = 10;
       this.defense = 0;
       this.attack = 3;
       this.special = 5;
     } else if (this.type === "knight") {
-      this.health = 10;
       this.defense = 3;
       this.attack = 3;
       this.special = 4;
     } else if (this.type === "rogue") {
-      this.health = 10;
       this.defense = 1;
       this.attack = 2;
       this.special = 6;
